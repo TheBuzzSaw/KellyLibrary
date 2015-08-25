8ed3b917199a4934e2c9b6d5e7f30c84c3168e50
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2015-08-25 14:55:37 -0600

    Began documenting the interface for DataMap.

--- include/DataMap.hpp
+++ include/DataMap.hpp
@@ -9,6 +9,28 @@
 
 namespace Kelly
 {
+    /// DataMap is a data-oriented associative array. Its focus is on POD
+    /// types, which means DataMap never invokes the keys' or values'
+    /// respective constructors, destructors, or assignment operators. The data
+    /// is laid out in a cache-friendly manner. Whereas traditional map
+    /// implementations often store key/value pairs together (KVKVKVKV),
+    /// DataMap keeps keys and values in separate containers (KKKKVVVV). This
+    /// provides several key tradeoffs:
+    ///
+    /// (1) Keys are packed in a cache-friendly manner. While not as fast as a
+    /// hash-based lookup, binary searches here are significantly faster than
+    /// traversing a cache-hostile binary tree.
+    ///
+    /// (2) Keys are kept in order. This makes insertion generally slower, but
+    /// if the keys are ever incrementing, pairs are always inserted on the
+    /// back, which ends up being significantly faster. Insertion and removal
+    /// near the front is the worst case.
+    ///
+    /// (3) Keys and values can be traversed separately. Values are packed in a
+    /// cache-friendly way the same as keys are. DataMap exposes functions for
+    /// accessing the two respective arrays for iteration. This positions
+    /// DataMap to be extremely powerful for cases where insertion/removal are
+    /// infrequent and where iteration happens often.
     template<typename K, typename V> class DataMap
     {
         static constexpr ptrdiff_t KeyValueSize = sizeof(K) + sizeof(V);
@@ -17,6 +39,11 @@ namespace Kelly
         ptrdiff_t _capacity;
         ptrdiff_t _count;
 
+        /// Private utility function to expand the memory block to be able to
+        /// hold more key/value pairs. After resizing the block, any existing
+        /// values are repositioned to correspond with the new capacity.
+        ///
+        /// capacity -- The minimum number of key/value pairs to make room for.
         void Expand(ptrdiff_t capacity)
         {
             auto proposedCapacity = std::max<ptrdiff_t>(capacity, 8);
@@ -33,6 +60,14 @@ namespace Kelly
             _capacity = proposedCapacity;
         }
 
+        /// Private utility function to locate the address of a particular
+        /// value based on a given key. This serves as the implementation for
+        /// the const and non-const variants of TryGet.
+        ///
+        /// key -- The desired value's corresponding key.
+        ///
+        /// Returns a valid pointer to the value that corresponds with the key
+        /// parameter. Otherwise returns null indicating the key was not found.
         V* FindValue(K key) const noexcept
         {
             auto keys = (K*)_block;
@@ -50,6 +85,15 @@ namespace Kelly
             return nullptr;
         }
 
+        /// Private utility function to create a new key/value pair. It is
+        /// assumed that this function's caller already did the work to
+        /// determine where in the collection the insertion needs to take
+        /// place. Used in AlwaysGet, Add, and Set.
+        ///
+        /// key -- The new pair's key.
+        /// position -- The collection ordinal to begin shifting elements.
+        ///
+        /// Returns a reference to the newly created pair's value.
         V& Insert(K key, ptrdiff_t position)
         {
             if (_count == _capacity) Expand(_capacity * 2);
@@ -67,6 +111,9 @@ namespace Kelly
         }
 
     public:
+
+        /// Constructs an empty DataMap object. No memory is allocated until a
+        /// key/value pair is added.
         DataMap() noexcept
             : _block(nullptr)
             , _capacity(0)
@@ -74,6 +121,11 @@ namespace Kelly
         {
         }
 
+        /// Move-constructs a DataMap object. The other DataMap object is left
+        /// with no allocated memory (as if it were newly constructed using
+        /// DataMap()).
+        ///
+        /// other -- DataMap object to have its content taken.
         DataMap(DataMap&& other) noexcept
             : _block(other._block)
             , _capacity(other._capacity)
@@ -84,6 +136,11 @@ namespace Kelly
             other._count = 0;
         }
 
+        /// Copy-constructs a DataMap object. Memory is only allocated if the
+        /// other DataMap object has key/value pairs in it. All corresponding
+        /// pairs are copied over in that case.
+        ///
+        /// other -- DataMap object to have its content copied.
         DataMap(const DataMap& other)
             : DataMap()
         {
@@ -104,6 +161,9 @@ namespace Kelly
             }
         }
 
+        /// Destroys this DataMap object. Any allocated memory is released as a
+        /// single block. No other destructors are called as this is a
+        /// POD-focused collection.
         ~DataMap() noexcept
         {
             free(_block);
@@ -217,16 +277,20 @@ namespace Kelly
         void Set(K key, V value)
         {
             auto keys = (K*)_block;
-            auto values = (V*)(keys + _capacity);
             auto endKey = keys + _count;
 
             auto k = std::lower_bound(keys, endKey, key);
             auto position = k - keys;
 
             if (k != endKey && *k == key)
+            {
+                auto values = (V*)(keys + _capacity);
                 values[position] = value;
+            }
             else
+            {
                 Insert(key, position) = value;
+            }
         }
 
         bool Remove(K key) noexcept
@@ -255,11 +319,6 @@ namespace Kelly
         void Reserve(ptrdiff_t capacity)
         {
             if (capacity > _capacity) Expand(capacity);
-        }
-
-        View<K> Keys() noexcept
-        {
-            return { (K*)_block, _count };
         }
 
         View<V> Values() noexcept
